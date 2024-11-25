67293224567fa034ac57e4c0c75c458597c2ba10
shreyam-dharmvans <sdharmvans@gmail.com>
2024-11-25 23:17:22 +0530

    save-user-fields

---
+++ src/app/api/(user)/save-user-fields/route.ts
@@ -0,0 +1,12 @@
+export async function POST(request: Request) {
+    try {
+        const { other_details,
+            progress: number
+            date_created: Date
+        goal: string
+        current_status: string
+        profile_picture: string
+        } catch (error) {
+
+        }
+    }
