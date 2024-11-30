7fa94bc91649ed707eeed5f5a6dc0c1ff106e2b1
KunalNasa <kunalnasa.dev@gmail.com>
2024-11-30 23:20:00 +0530

    header and footer after login page

--- src/components/PageEnd.tsx
+++ src/components/PageEnd.tsx
@@ -9,18 +9,19 @@ import { FaLinkedin } from "react-icons/fa";
 const PageEnd = () => {
   return (
     <div>
-      <Separator className='w-full'/>
+      <Separator className='w-full bg-gray-400'/>
       <div className='MainContainer flex items-center justify-between'>
         <div className='p-1 flex items-center'>
           <Image width={75} height={75} src="/Logo.svg" alt="" />
-          <p className='my-5 text-gray-500'>
+          <p className='my-5 text-sm text-gray-500'>
             © 2024 SkillForge. All rights reserved.
           </p>
         </div>
-        <div className='flex gap-5 mx-4'>
+        <div className='flex md:flex-col gap-2 md:gap-5 mx-4'>
           <p className='my-5 text-gray-400'>
             Connect :
           </p>
+          <div className='flex md:gap-5'>
           <Link 
                 href="https://twitter.com"
                 target="_blank"
@@ -45,6 +46,7 @@ const PageEnd = () => {
                 >
                 <SlSocialGithub />
           </Link>
+          </div>
         </div>
       </div>
     </div>
